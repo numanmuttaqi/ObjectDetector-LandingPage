96f7f2b1110f65192a683f397386c90aed465dd6
numanmuttaqi <numan.muttaqi@tum.de>
2024-03-12 12:29:09 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function show(){
+    document.querySelector('.hamburger').classList.toggle('open');
+    document.querySelector('.navigation-left').classList.toggle('active');
+}
+
+
+function showright(){
+    document.querySelector('.plus').classList.toggle('open');
+    document.querySelector('.navigation-right').classList.toggle('active');
+}
