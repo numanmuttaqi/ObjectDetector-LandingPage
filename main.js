db063280b0ad44bdbab1ee9dc4980b4b9a8648be
numanmuttaqi <numan.muttaqi@tum.de>
2024-03-12 21:30:41 +0100

    Update on left navigation

--- main.js
+++ main.js
@@ -8,3 +8,15 @@ function showright(){
     document.querySelector('.plus').classList.toggle('open');
     document.querySelector('.navigation-right').classList.toggle('active');
 }
+
+const weight = document.getElementById('weight')
+const weightRangeValue = document.getElementById('weight-range-value');
+weight.addEventListener('input', function(){
+    weightRangeValue.textContent = weight.value;
+})
+
+const zoom = document.getElementById('zoom');
+const zoomRangeValue = document.getElementById('zoom-range-value');
+zoom.addEventListener('input', function(){
+    zoomRangeValue.textContent = zoom.value;
+})
